01d433b33f509696e3a100184fbdf5d651394f8b
traycn <tracy@traycn.co>
2019-01-27 13:10:21 -0800

    Prefabs for icons and intialization of iconObjects in scene

---
+++ Recharge/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] iconObject;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    ////public int? iconHits = null;
+
+    List<int> spawnPointArray = new List<int>();
+    ////List<int> iconObjectArray = new List<int>();
+
+    // Start is called before the first frame up
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        //START: Default show all spawn on initialization of the game
+        if (spawnPointArray.Count != 8)
+        {
+            int spawnIconObject = 0;
+
+            for (int i = 0; i < iconObject.Length; i++)
+            {
+                spawnIconObject = i;
+
+                Instantiate(iconObject[spawnIconObject], spawnPoints[spawnIconObject].position, spawnPoints[spawnIconObject].rotation);
+
+                spawnPointArray.Add(i);
+            }
+        } else //GAMEPLAY: track iconObjects that have been hit
+        {
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+    //if(playerHealth.currentHealth <= 0f)
+    //{
+    //          return;
+    //}
+
+    //int[] spawnPointArray = null;
+    //int[] iconObjectArray = null;
+
+    //int spawnPointIndex = 0;
+    //int iconObjectIndex = 0;
+
+    //if (iconHits == null)
+    //{
+    //    for (int i = 0; i < 4; i++)
+    //    {
+
+    ////spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+    //   if (spawnPointArray.Contains(spawnPointIndex))
+    //   {
+    //       spawnPointIndex = Random.Range(0, spawnPoints.Length);
+    //   }
+
+    //   if (iconObjectArray.Contains(iconObjectIndex))
+    //   {
+    //       iconObjectIndex = Random.Range(0, iconObject.Length);
+    //   }
+
+
+
+    //spawnPointArray.Add(spawnPointIndex);
+    //iconObjectArray.Add(iconObjectIndex);
+
+    //    }
+    //} else
+    //{
+
+    //}
+
+    //int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+    //int iconObjectIndex = Random.Range(0, iconObject.Length);
+
+
+    /// <summary>
+    /// 
+    /// GOOD OBJECTS vs BAD OBJECTS
+    /// 
+    /// Good Objects: 
+    /// - food
+    /// - game controller
+    /// - music
+    /// - plane
+    /// 
+    /// Bad Objects:
+    /// - ambulance
+    /// - sun
+    /// - tax
+    /// - work
+    /// 
+    /// 
+    /// 
+    /// </summary>
+}
