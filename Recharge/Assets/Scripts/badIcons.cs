ae3dcc33c79b1eb9bf846c38793e7da6597da58c
citrusapple <wng2014@gmail.com>
2019-02-07 20:05:15 -0800

    added documentation explaining code in BadIcons.cs that restores player at V2 zero/ home when colliding with these objects

--- Recharge/Assets/Scripts/badIcons.cs
+++ Recharge/Assets/Scripts/badIcons.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class badIcons : MonoBehaviour
 {
     // Start is called before the first frame update
+    //used on bad icons, when colliding with player, player restores position to vector2.zero/ Home.
     void OnCollisionEnter2D(Collision2D collision)
     {
         collision.gameObject.transform.position = Vector2.zero;
